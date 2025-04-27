0a0d5776b4e6f46128b3edb2ec66002c1c7c3941
gabenicholson <gabenicholson@uchicago.edu>
2025-04-27 17:29:17 -0400

    update session id recovery

--- public/analytics.js
+++ public/analytics.js
@@ -117,6 +117,27 @@ class Analytics {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload)
       });
+      if (!response.ok) {
+        if (json.error?.code === '23503') {
+          console.warn("session not found but in cookies, creating new one in database...")
+          const timestamp = new Date().toISOString();
+          const payload = {
+            session_id: sessionId,
+            first_seen_timestamp: timestamp,
+            last_seen_timestamp: timestamp
+          }
+          const response = await fetch('/api/analytics/user_session', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(payload)
+          });
+          if (response.ok) {
+            console.log("successfully added sessionId into db")
+          }
+        } else {
+          console.error("session id in cookie but error isnt handled...")
+        }
+      }
     }
     return sessionId;
   }
